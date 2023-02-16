7472241d64aea1b743c61de4b9ece87dcffb71d8
Louis Perry <louis.perry3@gmail.com>
2023-02-15 18:15:54 -0800

    updated Header

---
+++ cinetrail/src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import Homepage from './pages/Homepage/Homepage';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import ThemeContextProvider from './contexts/ThemeContext';
+
+
+function App() {
+  return (
+    <div className="App">
+      <ThemeContextProvider>
+        <Header />
+        <Homepage />
+        <Footer />
+      </ThemeContextProvider>
+    </div>
+  );
+}
+
+export default App;
